5f951183760ad82ab36bea955bbe45280b7a9526
Umutcan Ceyhan <umutcanceyhan@gmail.com>
2022-03-30 17:44:17 +0300

    First commit

---
+++ myshell.c
@@ -0,0 +1,24 @@
+#define command[100]
+
+int main()
+{
+    /*
+    print "myshell>" 
+    read command line 
+    parse command 
+    if the command is built-in 
+    execute command //in your implementation
+    else 
+    fork a child 
+    if child 
+    execute command //calls execvp 
+    else if not background process
+    wait for the child
+    */
+
+	while (1) {
+		
+	}
+	return 0;
+    
+}
